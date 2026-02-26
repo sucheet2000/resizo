6832c31aa67adac3456a62e03a948006a7257499
BOPPANA SAI SUCHEET <83731527+sucheet2000@users.noreply.github.com>
2026-02-26 14:28:01 -0800

    seo: add metadata, sitemap, robots, and Open Graph tags

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+        },
+        sitemap: 'https://resizo-woad.vercel.app/sitemap.xml',
+    };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://resizo-woad.vercel.app',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1.0,
+        },
+    ];
+}
