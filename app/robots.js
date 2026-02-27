e1514086ff2c366088aa090485fc9712f9223da5
BOPPANA SAI SUCHEET <83731527+sucheet2000@users.noreply.github.com>
2026-02-26 16:03:54 -0800

    seo: add about page to sitemap

--- app/robots.js
+++ app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://resizo-woad.vercel.app/sitemap.xml',
+        sitemap: 'https://www.resizo.net/sitemap.xml',
     };
 }
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,16 @@
 export default function sitemap() {
     return [
         {
-            url: 'https://resizo-woad.vercel.app',
+            url: 'https://www.resizo.net',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 1.0,
+        },
+        {
+            url: 'https://www.resizo.net/about',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
         },
     ];
 }
